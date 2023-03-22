16bd77c5066302911ced96e464cd62381af9564c
Moti-Sultan <motisultannur@gmail.com>
2023-03-22 21:18:33 +0100

    Menu with router without resolved path

--- src/App.js
+++ src/App.js
@@ -3,24 +3,19 @@ import './App.css';
 import Navbar from './navbar';
 import Accueil from './pages/Accueil';
 import Mesfavoris from './pages/Mesfavoris';
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
-  let component
-  switch (window.location.pathname) {
-    case '/':
-      component = <Accueil />
-      break;
-    case '/Mesfavoris':
-      component = <Mesfavoris />
-      break;
-    default:
-      break;
-  }
+
   return (
     <>
       <Navbar />
       <div className="container">
-        {component}
+        <Routes>
+          <Route path="/" element={<Accueil />} />
+          <Route path="/Mesfavoris" element={<Mesfavoris />} />
+        </Routes>
       </div>
     </>
   )
--- src/navbar.js
+++ src/navbar.js
@@ -1,21 +1,15 @@
-
+import { Link } from "react-router-dom"
 export default function Navbar() {
     return <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-            <a className="navbar-brand" href="/">ALLO-MOVIE</a>
+            <Link className="navbar-brand" to="/">ALLO-MOVIE</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <CustomLink href="/"> Accueil </CustomLink>
-                    <CustomLink href="/Mesfavoris"> Mesfavoris </CustomLink>
-                    {/* <li className="nav-item">
-                        <a className="nav-link active" href="/">Accueil</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/Mesfavoris">Mes Favoris</a>
-                    </li> */}
+                    <CustomLink to="/"> Accueil </CustomLink>
+                    <CustomLink to="/Mesfavoris"> Mesfavoris </CustomLink>
                 </ul>
                 {/* <form className="d-flex">
                     <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search">
@@ -26,12 +20,12 @@ export default function Navbar() {
     </nav>
 }
 
-function CustomLink({ href, children, ...props }) {
+function CustomLink({ to, children, ...props }) {
     const path = window.location.pathname
     return (
         <>
             <li className="nav-item">
-                <a className={path === href ? "active nav-link" : "nav-link"} href={href} {...props}> {children}</a>
+                <Link className={path === to ? "active nav-link" : "nav-link"} to={to} {...props}> {children}</Link>
             </li>
         </>
     )

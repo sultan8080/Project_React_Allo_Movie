c6042c3e9e9fe4af1bac07151f4b882aaccdbcd4
Moti-Sultan <motisultannur@gmail.com>
2023-03-22 17:20:15 +0100

    Menu created

---
+++ src/pages/Accueil.js
@@ -0,0 +1,10 @@
+export default function Accueil() {
+    return (
+        <>
+            <div className="text-center">
+                This is my HOME PAGE
+            </div>
+        </>
+    )
+
+}
---
+++ src/pages/Mesfavoris.js
@@ -0,0 +1,11 @@
+export default function Mesfavoris() {
+    return (
+        <>
+            <div className="mx-auto">
+                This is my favourites;
+
+            </div>
+        </>
+    )
+
+}

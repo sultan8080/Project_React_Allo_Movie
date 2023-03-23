73d0c659d209b7abf977bf83aeb84408d8455417
Moti-Sultan <motisultannur@gmail.com>
2023-03-23 22:07:49 +0100

    navigation ok for film details

--- src/components/DisplayFilmsList.jsx
+++ src/components/DisplayFilmsList.jsx
@@ -5,41 +5,48 @@ const imgPath = "https://image.tmdb.org/t/p/w500/";
 
 function FilmsListDisplay({ film }) {
   return (
-    <div className="col-lg-3 col-md-4 col-sm-6 col-12">
-      <div className="card shadow-sm mb-3">
-        <img
-          className="card-img-top"
-          src={imgPath + film.poster_path}
-          alt="Card image cap"
-        />
-        <div className="card-body">
-          <h6 className="card-title">{film.title}</h6>
-          <p className="card-text"> Date de Sortie: {film.release_date} </p>
-          <hr />
-          {/* href="#" className="btn btn-primary">Go somewhere */}
-          <div className="d-flex justify-content-around">
-            <Link to={`/FilmDetails/${film.id}`} state={{ film }}>
-              <button type="button" className="btn btn-warning btn-sm">
-                En savoir Plus{" "}
-              </button>
-            </Link>
+    <div className=" col-lg-4 col-sm-6 col-12 ">
+      <div className="card mb-3 shadow-sm">
+        <div className="row g-0">
+          <div className="col-md-5">
+            <img
+              className="card-img-top"
+              src={imgPath + film.poster_path}
+              alt="Card image cap"
+            />
+          </div>
+          <div className="col-md-7">
+            <div className="card-body">
+              <h6 className="card-title fw-bold">{film.title}</h6>
+              <p className="card-text"> Date de Sortie: {film.release_date} </p>
+              <br />
+              <br />
+              <br />
+              <div className="d-flex-colum justify-content-center align-items-center">
+                <Link to={`/FilmDetails/${film.id}`} state={{ film }}>
+                  <button
+                    type="button"
+                    className="btn btn-outline-dark fw-bold tex btn-sm mb-2 w-100"
+                  >
+                    En savoir Plus{" "}
+                  </button>
+                </Link>
 
-            <button type="button" className="btn btn-warning btn-sm">
-              En Favoris{" "}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="11"
-                height="11"
-                fill="red"
-                className="bi bi-heart-fill"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
-                />
-              </svg>
-            </button>
+                <button type="button" className="btn btn-warning btn-sm w-100">
+                  En Favoris
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="11"
+                    height="11"
+                    fill="red"
+                    className="bi bi-heart-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+                  </svg>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/FilmDetails.jsx
+++ src/pages/FilmDetails.jsx
@@ -10,15 +10,40 @@ function FilmDetails() {
 
   const { film } = state || {}; // <-- unpack the film from state
 
-  console.log(film);
+  const imgPath = "https://image.tmdb.org/t/p/w500/";
 
   return film ? (
-    <div>
-      <h1>{film.id}</h1>
-      <h1>{film.title}</h1>
+    <div className="container mt-4 ">
+      <div className="card shadow-lg mb-3">
+        <div className="card-body">
+          <h1 className="text-center h1">{film.title}</h1>
+          <hr />
+          <h4 className="card-title text-center">
+            Date de Sortie: {film.release_date}
+          </h4>
+          <p className="card-text">{film.overview}</p>
+          <div className="d-flex justify-content-around">
+            <p className="card-text fw-bold text-secondary">
+              {" "}
+              Ratings : {film.vote_average}
+            </p>
+            <p className="card-text fw-bold text-secondary">
+              Vote Totale : {film.count}
+            </p>
+            <p className="card-text fw-bold text-secondary">
+              Popularité : {film.popularity}
+            </p>
+          </div>
+        </div>
+        <img
+          className="card-img-top"
+          src={imgPath + film.poster_path}
+          alt="Card image cap"
+        />
+      </div>
     </div>
   ) : (
-    "No item matched/found."
+    "No film found."
   );
 }
 export default FilmDetails;

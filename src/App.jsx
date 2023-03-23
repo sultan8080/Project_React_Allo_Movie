fec8a578e66ebccc691756643a9d38026841904a
Moti-Sultan <motisultannur@gmail.com>
2023-03-23 20:53:04 +0100

    Bug fixed, routing ok with single film details

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './navbar';
-import Accueil from './pages/Accueil';
-import Mesfavoris from './pages/Mesfavoris';
-import { Route, Routes } from 'react-router-dom';
-import FilmDetails from './pages/FilmDetails';
-import { useLocation } from 'react-router-dom';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./navbar";
+import Accueil from "./pages/Accueil";
+import Mesfavoris from "./pages/Mesfavoris";
+import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import FilmDetails from "./pages/FilmDetails";
+import { useLocation } from "react-router-dom";
 
 function App() {
   const { state } = useLocation(); // <-- access route state
-  const { film } = state || {};
+  const { film } = state || {}; // <-- unpack the film from state
+  const id = () => {
+    return film.id;
+  };
 
   return (
     <>
@@ -19,11 +22,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Accueil />} />
           <Route path="/Mesfavoris" element={<Mesfavoris />} />
-          <Route path="/FilmDetails/{film.id}" element={<FilmDetails />} />
+          <Route path="/FilmDetails/:id" element={<FilmDetails />} />
         </Routes>
       </div>
     </>
-  )
+  );
 }
 
 export default App;
--- src/pages/FilmDetails.jsx
+++ src/pages/FilmDetails.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-
+import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function FilmDetails() {
-    const { state } = useLocation(); // <-- access route state
+  const { id } = useParams();
 
-    const { film } = state || {}; // <-- unpack the film from state
+  const { state } = useLocation(); // <-- access route state
 
-    console.log(film);
+  const { film } = state || {}; // <-- unpack the film from state
 
+  console.log(film);
 
-
-    return film ? (
-        <div>
-            <h1>{film.title}</h1>
-        </div>
-    ) : "No item matched/found.";
-
+  return film ? (
+    <div>
+      <h1>{film.id}</h1>
+      <h1>{film.title}</h1>
+    </div>
+  ) : (
+    "No item matched/found."
+  );
 }
 export default FilmDetails;
-
